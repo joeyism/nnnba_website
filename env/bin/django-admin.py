d949d75932706e53f339318bcade2c3172b3866f
joeyism <sham.joey@gmail.com>
2017-07-29 12:41:17 -0400

    fixed it so things are returned instead of printed

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/dante/Documents/Programming/python/nnnba_website/env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
