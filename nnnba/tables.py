2bc58731c26a672e1973f9395c4e7d7557e2173c
joeyism <sham.joey@gmail.com>
2017-07-31 23:18:01 -0400

    Added currency column

--- nnnba/tables.py
+++ nnnba/tables.py
@@ -2,9 +2,15 @@ import django_tables2 as tables
 from .models import Player
 from django_tables2.utils import A 
 
+class CurrencyColumn(tables.Column):
+    def render(self,value):
+        return '${:,.2f}'.format(value)
+
 class MainPlayersTable(tables.Table):
     name = tables.LinkColumn('nnnba:main_player', text=lambda record: record.name, args=[A('pk')], orderable=True)
-    class Meta:
+    paid = CurrencyColumn()
+    projected_salaries = CurrencyColumn()
+    class Meta: 
         model = Player
         attrs = {'class': 'paleblue'}
         fields = ("name", "paid", "projected_salaries")
